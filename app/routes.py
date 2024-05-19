bb774587e0db10f9d7974e11757a0a9b55415aad
Brad <mrbradley.ferguson@gmail.com>
2024-05-19 19:15:17 -0400

    finished ch8 -- starting ch9

--- app/routes.py
+++ app/routes.py
@@ -1,44 +1,49 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
+from app.models import User, Post
 import sqlalchemy as sa
 from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlsplit
 from datetime import datetime, timezone
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+  form = PostForm()
+  if form.validate_on_submit():
+    post = Post(body=form.post.data, author=current_user)
+    db.session.add(post)
+    db.session.commit()
+    flash('Your post is now live!')
+    return redirect(url_for('index'))
+  posts = db.session.scalars(current_user.following_posts()).all()
+  return render_template("index.html", title='Home Page', form=form, posts=posts)
+
+@app.route('/explore')
+@login_required
+def explore():
+  query = sa.select(Post).order_by(Post.timestamp.desc())
+  posts = db.session.scalars(query).all()
+  return render_template('index.html', title='Explore', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or urlsplit(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect(url_for('login'))
+    login_user(user, remember=form.remember_me.data)
+    next_page = request.args.get('next')
+    if not next_page or urlsplit(next_page).netloc != '':
+      next_page = url_for('index')
+      return redirect(next_page)
+  return render_template('login.html', title='Sign In', form=form)
   
 @app.before_request
 def before_request():
@@ -72,12 +77,13 @@ def user(username):
     {'author': user, 'body': 'Test post #1'},
     {'author': user, 'body': 'Test post #2'}
   ]
-  return render_template('user.html', user=user, posts=posts)
+  form = EmptyForm()
+  return render_template('user.html', user=user, posts=posts, form=form)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
@@ -89,3 +95,43 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
   
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == username))
+        if user is None:
+            flash(f'User {username} not found.')
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash(f'You are following {username}!')
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+  form = EmptyForm()
+  if form.validate_on_submit():
+    user = db.session.scalar(
+      sa.select(User).where(User.username == username))
+    if user is None:
+      flash(f'User {username} not found.')
+      return redirect(url_for('index'))
+    if user == current_user:
+      flash('You cannot unfollow yourself!')
+      return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash(f'You are not following {username}.')
+    return redirect(url_for('user', username=username))
+  else:
+    return redirect(url_for('index'))
