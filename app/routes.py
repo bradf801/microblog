f3e7ab389d3cafcba1fed167550ab48a531b0c9d
Brad <mrbradley.ferguson@gmail.com>
2024-06-06 13:42:59 -0400

    Saving progress on email section

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm
 from app.models import User, Post
+from app.email import send_password_reset_email
 import sqlalchemy as sa
 from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlsplit
@@ -157,3 +158,18 @@ def explore():
       if posts.has_prev else None
   return render_template("index.html", title='Explore', posts=posts.items,
                           next_url=next_url, prev_url=prev_url)
+  
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.email == form.email.data))
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
